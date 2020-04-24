dfcca20185905bb631e84d90409ce25985dff58b
Amalhotra29 <aryan.malhotra2000@yahoo.in>
2020-04-24 09:03:14 +0530

    Grenade Throwing added

---
+++ Assets/Scripts/GrenadeThrower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrower : MonoBehaviour
+{
+    private float currentGrenadeCount;
+    public float ThrowForce = 16;
+    public float MaxGrenadeCount = 10;
+    public GameObject Grenade;
+    void Start()
+    {
+        currentGrenadeCount = MaxGrenadeCount;
+    }
+    void ThrowGrenade()
+    {
+        GameObject ThrownGrenade = Instantiate(Grenade, transform.position, transform.rotation);
+        Rigidbody rb = ThrownGrenade.GetComponent<Rigidbody>();
+        rb.AddForce((transform.forward * ThrowForce), ForceMode.VelocityChange);
+        currentGrenadeCount--;
+    }
+    void Update()
+    {
+        if (currentGrenadeCount >= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                ThrowGrenade();
+            }
+        }
+    }
+}
